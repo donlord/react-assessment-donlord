fc6eb507d9da4bdd5a040e1bb5d3023602cfa145
donny <dramier17@gmail.com>
2018-09-13 16:41:07 -0500

    fixed chalkboard

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Switch, Route, NavLink } from 'react-router-dom'
+import Typing from 'react-typing-animation'
 import './About.css'
 
 const About = props => (
@@ -10,14 +11,16 @@ const About = props => (
     <div className='chalkboard'>
       <img src='./src/Assets/dj3.png' className='dj' />
       <ul className='list'>
-        <li>Currently live in Memphis, TN and I grew up in New Jersey</li>
-        <li>Rhodes College '17 Computer Science Major</li>
-        <li>Began Coding with the help of my roommates</li>
-        <li>Family Oriented - 3 musketeers</li>
-        <li>Salvation Army Lifeguard - CPR certified</li>
-        <li> Active lifestyle - Avid Sports fan, novice bodybuilder</li>
-        <li> Animal lover - Shelter Volunteer and Dog Owner</li>
+        <Typing>
+          <li>Currently live in Memphis, TN and I grew up in New Jersey</li>
 
+          <li>Rhodes College '17 Computer Science Major</li>
+          <li>Began Coding with the help of my roommates</li>
+          <li>Family Oriented - 3 musketeers</li>
+          <li>Salvation Army Lifeguard - CPR certified</li>
+          <li> Active lifestyle - Avid Sports fan, novice bodybuilder</li>
+          <li> Animal lover - Shelter Volunteer and Dog Owner</li>
+        </Typing>
       </ul>
       <img src='./src/Assets/amber.jpg' className='amber' />
     </div>
